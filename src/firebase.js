d584852a82255c85fad4ff98256eef28d4b81210
BuildTools <unconfigured@null.spigotmc.org>
2022-12-10 19:13:07 +0900

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBWCvAm8t-imtgY5eQp5ed7yALfP484zM0",
+    authDomain: "ywitter-yuhan8954.firebaseapp.com",
+    projectId: "ywitter-yuhan8954",
+    storageBucket: "ywitter-yuhan8954.appspot.com",
+    messagingSenderId: "4213241372",
+    appId: "1:4213241372:web:b2e37e2769b809fcb991d9"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
